c08bfd36e4b14d12104c4950cf63b59e225d9290
markus-senger <senger1995@gmail.com>
2023-05-14 22:32:37 +0200

    add event for collider check so the ml agent can punish when hitting a wall

--- Assets/Scripts/ColliderCheck.cs
+++ Assets/Scripts/ColliderCheck.cs
@@ -1,14 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ColliderCheck : MonoBehaviour
 {
+    public event EventHandler OnCarColliderEnter;
+    public event EventHandler OnCarColliderStay;
+
     public static bool collisionFlag;
 
     private void OnCollisionEnter(Collision collision)
     {
         collisionFlag = true;
+        OnCarColliderEnter?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        OnCarColliderStay?.Invoke(this, EventArgs.Empty);
     }
 
     private void OnCollisionExit(Collision collision)
